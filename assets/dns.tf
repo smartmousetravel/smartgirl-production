d80d0332c789ee3693301b01f175667b7eb79749
Lucas <lucas@smartmousetravel.com>
2021-05-31 01:51:34 +0000

    assets: Add NS and SOA DNS records
    This might be a mistake; it seems like these records are created automatically
    when you create a managed zone. So, you may just have to manually update the
    top-level NS pointers whenever the zone gets recreated.

--- assets/dns.tf
+++ assets/dns.tf
@@ -4,6 +4,27 @@ resource "google_dns_managed_zone" "smartgirltravel" {
     description = "smartgirltravel.com"
 }
 
+resource "google_dns_record_set" "smartgirltravel_ns" {
+    managed_zone = google_dns_managed_zone.smartgirltravel.name
+    name = google_dns_managed_zone.smartgirltravel.dns_name
+    type = "NS"
+    rrdatas = [
+        "ns-cloud-b1.googledomains.com.",
+        "ns-cloud-b2.googledomains.com.",
+        "ns-cloud-b3.googledomains.com.",
+        "ns-cloud-b4.googledomains.com.",
+    ]
+    ttl = 21600
+}
+
+resource "google_dns_record_set" "smartgirltravel_soa" {
+    managed_zone = google_dns_managed_zone.smartgirltravel.name
+    name = google_dns_managed_zone.smartgirltravel.dns_name
+    type = "SOA"
+    rrdatas = ["ns-cloud-b1.googledomains.com. cloud-dns-hostmaster.google.com. 1 21600 3600 259200 300"]
+    ttl = 21600
+}
+
 resource "google_dns_record_set" "smartgirltravel_www_prod_1_a" {
     managed_zone = google_dns_managed_zone.smartgirltravel.name
     name = "www-prod-1.smartgirltravel.com."
